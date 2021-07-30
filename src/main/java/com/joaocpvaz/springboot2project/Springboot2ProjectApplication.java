53966c95deb6913bce66e77785d57cf6b1d5f6e7
Joao Vitor Vaz <joaov.chagas.vaz@gmail.com>
2021-07-30 14:20:47 -0300

    first commit

---
+++ src/main/java/com/joaocpvaz/springboot2project/Springboot2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.joaocpvaz.springboot2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2ProjectApplication.class, args);
+	}
+
+}
